40d7107b7a3b006248b099018b34d6c054935d11
JamalGuyo <jamalguyo08@gmail.com>
2020-05-17 13:02:22 +0300

    create click listener for the squares. compare picked color with clicked color

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ let pickedColor = colors[3];
 let colorDisplay = document.querySelector('#colorDisplay');
 colorDisplay.textContent = pickedColor;
 
-// loop through all the divs. assign the colors to the div
+// loop through all the divs
 for(let i = 0, n = squares.length; i < n; i++){
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+    // add click listeners
+    squares[i].addEventListener('click', function(){
+        // get color of clicked square
+        let clickedColor = this.style.backgroundColor;
+        // compare with picked color
+        if(clickedColor === pickedColor){
+            alert('correct!');
+        }else{
+            alert('wrong!');
+        }
+    })
 }
